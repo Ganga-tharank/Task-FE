f73bf96a674d21fa3e31e33f80e18eda882c5ba7
Gangatharan k <154905189+Ganga-tharank@users.noreply.github.com>
2024-07-30 17:08:03 +0530

    Update authService.js

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api/users/';
+const API_URL = 'https://task-manager-server1-2.onrender.com/api/users/';
 
 const registerUser = async (userData) => {
     const response = await axios.post(API_URL + 'register', userData);
